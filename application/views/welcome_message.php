b5ca7dcc7bf5354013f174397503b9972ab7f534
jayeshsisodia06 <jayeshbrillsense@gmail.com>
2024-02-17 15:51:41 +0530

    ci3

---
+++ application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to PHP</title>
+    <style>
+        body {
+            background-color: #f0f0f0;
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            color: #333;
+            font-size: 36px;
+            text-align: center;
+            margin-top: 100px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to PHP</h1>
+</body>
+</html>
